b3ceb362304e7b17bfc65e514d5006500b0efe31
De'Yonte Wilkinson <tjtate@agorafinancial.com>
2014-10-06 16:31:13 -0400

    update admin file

---
+++ admin/co-authors2.admin.php
@@ -0,0 +1,17 @@
+<?<?php
+if( !class_exists('Co-Authors2-Admin') ){
+
+  /**
+   * Functionality for plugin to be executed while logged into the WordPress dashboard.
+   */
+  class Co-Authors2-Admin{
+    public function __construct(){
+
+    }
+
+    public function hooks(){
+
+    }
+
+  }
+}
