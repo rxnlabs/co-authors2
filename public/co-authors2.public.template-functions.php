14af58b9a8d1bb5ff2f03120a224ccd7cc9179c9
De'Yonte Wilkinson <dwilkinson@agorafinancial.com>
2014-10-10 16:01:26 -0400

    added functions to get the co-authors posts links (return) and display the co-authors plus links (echo).
    Tested out plugin on local install of AF site. Looks to be working and functioning like the Co-Authors Plus plugin. Will soon be ready to replace the plugin (with functionality that AF uses).
    
    https://app.asana.com/0/15421016095247/17229905476211

--- public/co-authors2.public.template-functions.php
+++ public/co-authors2.public.template-functions.php
@@ -43,3 +43,87 @@ function get_coauthors2( $post_id = '' ){
   else
     return;
 }
+
+/**
+ * Show link to co-author's pages.
+ * 
+ * Show link to the co-author's page where users can browse posts by author
+ * 
+ * @return void
+ */
+function coauthors2_posts_link( $post_id = '' ){
+  
+  if( get_coauthors2_posts_link($post_id)[0] instanceof WP_Error && ini_get('display_errors') == 1 ){
+    echo get_coauthors2_posts_link($post_id)[0]->get_error_message( $coauthors[0]->get_error_code() );
+  }else{
+    echo get_coauthors2_posts_link($post_id); 
+  }
+
+}
+
+/**
+ * Get link to the co-author's page.
+ * 
+ * Get link to the author's page where users can browse posts by author
+ * 
+ * @return string|WP_Error HTML with links to author's page and author's display name or instance of WP_Error if something went wrong
+ */
+function get_coauthors2_posts_link( $post_id = '' ){
+  global $post;
+  global $co_authors2;
+
+  $coauthors = array();
+  $default_author = '';
+
+  if( empty($post_id ) )
+    $post_id = $post->ID;
+
+  if( is_string($post_id) || is_numeric($post_id) ){
+    $post_id = $post_id;
+  }elseif( $post_id instanceof WP_Post ){
+    $default_author = $post->post_author;
+    $post_id = $post_id->ID;
+  }
+
+  $find_authors = get_coauthors2( $post_id );
+
+  if( is_array($find_authors) ){
+    foreach( $find_authors as $author ){
+      $author = get_userdata($author->ID);
+
+      if( $author != false ){
+        $link = get_author_posts_url($author->ID);
+        $name = $author->display_name;
+        $post_type = get_post_type( $post_id );
+        $post_type = get_post_type_object($post_type );
+
+        $coauthors[] = sprintf( '<a href="%1$s" title="%2$s by %3$s">%3$s</a>', $link, ucwords($post_type->labels->name), $name );
+      }
+    }
+  }else{
+    $default_author = ( !empty($default_author)?$default_author: get_post_field( 'post_author', $post_id, 'raw' ) );
+    
+    if( !$default_author instanceof WP_Error ){
+      $author = get_userdata($default_author);
+
+      $link = get_author_posts_url($author->ID);
+      $name = $author->display_name;
+      $post_type = get_post_type( $post_id );
+      $post_type = get_post_type_object($post_type );
+
+      $coauthors[] = sprintf( '<a href="%1$s" title="%2$s by %3$s">%3$s</a>', $link, ucwords($post_type->labels->name), $name );
+    }else{
+      $coauthors[] = $default_author;
+    }
+  }
+
+  if( count($coauthors) > 1 )
+    $coauthors[count($coauthors)-1] = __('and ').end($coauthors);
+
+  if( count($coauthors) != 0 && !$coauthors[0] instanceof WP_Error )
+    return implode( ', ', $coauthors );
+  elseif( $coauthors[0] instanceof WP_Error ){
+    return $coauthors[0];
+  }
+
+}
